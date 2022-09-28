e49860f70c92166e5f1586328dcd8ff5670b3e64
chesun <che.sun.1996@gmail.com>
2022-09-27 22:10:13 -0700

    updated code to randomly draw balls and htm button

---
+++ survey/pilot/button.js
@@ -0,0 +1,34 @@
+// js code for enabling button function
+
+// scenario 1 question 1
+jQuery("#buttonS1Q1").click(function() {
+  jQuery("#collapseS1Q1").toggle();
+});
+
+// scenario 1 question 2
+jQuery("#buttonS1Q2").click(function() {
+  jQuery("#collapseS1Q2").toggle();
+});
+
+// scenario 1 question 3
+jQuery("#buttonS1Q3").click(function() {
+  jQuery("#collapseS1Q3").toggle();
+});
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+// scenario 2 question 1
+jQuery("#buttonS2Q1").click(function() {
+  jQuery("#collapseS2Q1").toggle();
+});
+
+// scenario 2 question 2
+jQuery("#buttonS2Q2").click(function() {
+  jQuery("#collapseS2Q2").toggle();
+});
+
+// scenario 2 question 3
+jQuery("#buttonS2Q3").click(function() {
+  jQuery("#collapseS2Q3").toggle();
+});
--- survey/pilot/draw_ball.js
+++ survey/pilot/draw_ball.js
@@ -9,18 +9,67 @@ const nRedBallsRedUrn = [];
 const draw1RandNums = [];
 // arrawy for the second draw of random numbers in all scenarios
 const draw2RandNums = [];
+// arrawy for the third draw of random numbers in all scenarios
+const draw3RandNums = [];
+
+// array for the color of the selected urn in all scenarios
+const urnColors = [];
+// color of the first and second ball
+const ball1Colors = [];
+const ball2Colors = [];
 
 for (let i = 1; i <= 11; i++) {
   nRedUrns[i - 1] = parseInt(Qualtrics.SurveyEngine.getEmbeddedData('sce_' + i + '_n_red_urn'));
   nRedBallsRedUrn[i - 1] = parseInt(Qualtrics.SurveyEngine.getEmbeddedData('sce_' + i + '_n_red_ball_red_urn'));
 
-  // generate 2 random integeres from 1 to 10 inclusive for each scenario
-  draw1RandNums[i -1] = Math.floor(Math.random() * 10 + 1)
-  draw2RandNums[i -1] = Math.floor(Math.random() * 10 + 1)
+  // generate 3 random integeres from 1 to 10 inclusive for each scenario
+  draw1RandNums[i - 1] = Math.floor(Math.random() * 10 + 1);
+  draw2RandNums[i - 1] = Math.floor(Math.random() * 10 + 1);
+  draw3RandNums[i - 1] = Math.floor(Math.random() * 10 + 1);
 
   Qualtrics.SurveyEngine.setEmbeddedData('sce_' + i + '_rand_num_1', draw1RandNums[i -1]);
   Qualtrics.SurveyEngine.setEmbeddedData('sce_' + i + '_rand_num_2', draw2RandNums[i -1]);
+  Qualtrics.SurveyEngine.addEmbeddedData('sce_' + i + '_rand_num_3', draw3RandNums[i -1]);
+
+
+  // rule to determine the color of the selected urn
+  if (draw1RandNums[i - 1] <= nRedUrns[i - 1]) {
+    urnColors[i - 1] = "red";
+
+    // rule to determine the color of the ball if selected urn is red
+    if (draw2RandNums[i - 1] <= nRedBallsRedUrn[i - 1]) {
+      ball1Colors[i - 1] = "red";
+    } else {
+      ball1Colors[i - 1] = "blue";
+    }
+
+    if (draw3RandNums[i - 1] <= nRedBallsRedUrn[i - 1]) {
+      ball2Colors[i - 1] = "red";
+    } else {
+      ball2Colors[i - 1] = "blue";
+    }
+  } else {
+    urnColors[i - 1] = "blue";
+
+    // rule to determine the color of the ball if selected urn is blue
+    // number of blue balls in blue urn = number of red balls in red urn
+    if (draw2RandNums[i - 1] <= nRedBallsRedUrn[i - 1]) {
+      ball1Colors[i - 1] = "blue";
+    } else {
+      ball1Colors[i - 1] = "red";
+    }
+
+    if (draw3RandNums[i - 1] <= nRedBallsRedUrn[i - 1]) {
+      ball2Colors[i - 1] = "blue";
+    } else {
+      ball2Colors[i - 1] = "red";
+    }
+  }
+
+
+
+  Qualtrics.SurveyEngine.setEmbeddedData('sce_' + i + '_urn_color', urnColors[i -1]);
+  Qualtrics.SurveyEngine.setEmbeddedData('sce_' + i + '_ball_1_color', ball1Colors[i -1]);
+  Qualtrics.SurveyEngine.setEmbeddedData('sce_' + i + '_ball_2_color', ball2Colors[i -1]);
 
 }
-
-// note: next is to test color of the ball
